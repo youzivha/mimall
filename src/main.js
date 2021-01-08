8b7a949b4841349b73de1fa86f2e23597c38e8aa
cekh <shengzm2012@foxmail.com>
2021-01-09 00:28:22 +0800

    在main配置接口拦截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
+import App from './App.vue'
 
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  const res = response.data
+  if (res.status === 1) {
+    return res.data
+  } else if (res.status === 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
